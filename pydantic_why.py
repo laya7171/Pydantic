ccbe6358fd8895c9b48d21314fd476a1fd380951
Laya Pandey <layapandey7171@gmail.com>
2025-07-27 15:57:41 +0545

    pydantich_why added

---
+++ pydantic_why.py
@@ -0,0 +1,26 @@
+
+def insert_patient_data(name, age):
+
+    
+    print(name)
+    print(age)
+
+    #Demo code say 
+    print("Inserted into database")
+
+
+insert_patient_data("John Doe", "thirty") #now here comes the need for type checking 
+#the actual value thirty will be stored as a string in the database 
+
+#one solution (name:str, age:int) also known as type hinting
+#but type hinting is not enforced at runtime
+#it doesn't raise an error if the type is not correct
+
+#another solution 
+
+#if type(name)== str:
+    #insert into database
+#else: 
+#    raise TypeError("Type doesnt match")
+
+#this is not a good solution because it requires manual checking and is not scalable
